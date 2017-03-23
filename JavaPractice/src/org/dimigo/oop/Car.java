872ccc9f9149700929ed6b720b57a80eab23bc69
jjjj <jjjj@DESKTOP-AVQ4B9K>
2017-03-23 15:40:10 +0900

    aaaa

---
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ Car.java
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ *
+ * @author : jjjj
+ * @version : 1.0
+ */
+public class Car {
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	public String getCompany() {
+		return company;
+	}
+	public String getModel() {
+		return model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setCompany(String newCompany) {
+		company = newCompany;
+	}
+	public void setModel(String newModel) {
+		model = newModel;
+	}
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+	public void setMaxSpeed(int newMaxSpeed) {
+		maxSpeed = newMaxSpeed;
+	}
+	public void setPrice(int newPrice) {
+		price = newPrice;
+	}
+	
+	public void print() {
+		System.out.println("제조사명 : "+company);
+		System.out.println("모델명 : "+model);
+		System.out.println("색상 : "+color);
+		System.out.println("최대속도 : "+maxSpeed+"km");
+		System.out.println("가격 : "+String.format("%,d", price));
+	}
+}
---
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ CarTest.java
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ *
+ * @author : jjjj
+ * @version : 1.0
+ */
+public class CarTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c1,c2,c3;
+		c1=new Car();
+		c2=new Car();
+		c3=new Car();
+		
+		c1.setCompany("현대자동차");
+		c1.setModel("제네시스");
+		c1.setColor("검정색");
+		c1.setMaxSpeed(225);
+		c1.setPrice(50000000);
+		
+		c2.setCompany("기아자동차");
+		c2.setModel("K7");
+		c2.setColor("흰색");
+		c2.setMaxSpeed(246);
+		c2.setPrice(40000000);
+		
+		c3.setCompany("삼성자동차");
+		c3.setModel("SM7");
+		c3.setColor("회색");
+		c3.setMaxSpeed(200);
+		c3.setPrice(38000000);
+		
+		System.out.println("<<자동차 목록>>");
+		c1.print();
+		System.out.println();
+		c2.print();
+		System.out.println();
+		c3.print();
+
+	}
+
+}
