863b85755bf9790d7b5d41417fe4e0a3def11593
jjjj <jjjj@DESKTOP-AVQ4B9K>
2017-05-10 10:03:34 +0900

    asdf

---
+++ JavaPractice/src/org/dimigo/inheritance/Chinese.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ Korean.java
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 8.
+ * </pre>
+ *
+ * @author : jjjj
+ * @version : 1.0
+ */
+public class Chinese extends Person {
+	public Chinese(String name) {
+		super(name);
+	}
+	public void sayHello() {
+		System.out.println("니하오");
+	}
+	public void sayBye() {
+		System.out.println("짜이찌엔");
+	}
+	public String toString() {
+		return "저는 중국사람 "+super.getName()+"입니다.";
+	}
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ PersonTest.java
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 8.
+ * </pre>
+ *
+ * @author : jjjj
+ * @version : 1.0
+ */
+public class PersonTest {
+	
+	public static void main(String[] args) {
+		Person a = new Person("Tom");
+		Korean b = new Korean("홍길동");
+		Japanese c = new Japanese("다나카");
+		Chinese d = new Chinese("왕밍");
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println();
+		a.sayHello();
+		b.sayHello();
+		c.sayHello();
+		d.sayHello();
+		System.out.println();
+		a.sayBye();
+		b.sayBye();
+		c.sayBye();
+		d.sayBye();
+	}
+
+}
