1f45edb90cdd661c98d809f09d8424fe21222926
jjjj <jjjj@DESKTOP-AVQ4B9K>
2017-05-15 09:37:09 +0900

    asdf

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ SmartPhoneTest.java
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 15.
+ * </pre>
+ *
+ * @author : jjjj
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone[] phones = {
+				new IPhone("iPhone 7","애플",900000),
+				new Galaxy("갤럭시 S8","삼성",800000)
+		};
+		for(SmartPhone phone : phones) {
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction(phone);
+			phone.turnOff();
+		}
+	}
+}
